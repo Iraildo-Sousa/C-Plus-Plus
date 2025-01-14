cf57630cc633929aba63ef7d9f5b3745aa7dc5d7
Nodelay <121840258+Iraildo-Sousa@users.noreply.github.com>
2025-01-14 09:44:32 -0300

    Add files via upload

---
+++ Exercicios/Exercicio7.cpp
@@ -0,0 +1,15 @@
+/* Trabalhando com números de bases diferentes */
+
+#include <iostream>
+
+using namespace std;
+
+int main (int args, char ** argv) {
+
+    cout << 25 << endl; /* base decimal */
+    cout << 0x19 << endl; /* base hexadecimal */
+    cout << 031 << endl; /* base octal */
+    cout << 0b11001 << endl; /* base binária */
+
+    return 0;
+}
---
+++ Exercicios/HelloWorld.cpp
@@ -0,0 +1,14 @@
+/* Primeiro programa em C++ que mostra uma mensagem na tela */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hello, World!";
+    cout << endl;
+    cout << "Este é o meu primeiro programa em C++ neste curso." << endl;
+
+    return 0;
+}

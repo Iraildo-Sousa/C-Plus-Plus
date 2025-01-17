8a3ac187418ae1e1fcd8e2ed99af106df793bd0f
Nodelay <121840258+Iraildo-Sousa@users.noreply.github.com>
2025-01-17 11:09:53 -0300

    Add files via upload

---
+++ Códigos Diferenciados/Exercicio33.cpp
@@ -0,0 +1,44 @@
+/* Buzinar ou não buzinar? */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char ** argv) {
+
+    char entrada1, entrada2, entrada3, entrada4, entrada5, verdadeiro1 = 't', verdadeiro2 = 'r', verdadeiro3 = 'u', verdadeiro4 = 'e', falso1 = 'f', falso2 = 'a',falso3 = 'l',falso4 = 's', falso5 = 'e';
+    int entradaASCII, verdadeiroASCII, falsoASCII;
+    bool buzinar = false;
+
+    cout << "Gostaria de buzinar? ";
+    cin >> entrada1;
+    cin >> entrada2;
+    cin >> entrada3;
+    cin >> entrada4;
+
+
+
+    entradaASCII = entrada1 + entrada2 + entrada3 + entrada4;
+    verdadeiroASCII = verdadeiro1 + verdadeiro2 + verdadeiro3 + verdadeiro4;
+    falsoASCII = falso1 + falso2 + falso3 + falso4 + falso5;
+
+    if (entradaASCII == falso1 + falso2 + falso3 + falso4) {
+
+        cin >> entrada5;
+        entradaASCII = entradaASCII + entrada5;
+
+    }
+
+    /* Operador Ternário */
+
+    //cout << (entradaASCII == falso1 + falso2 + falso3 + falso4) ? scanf("%c", &entrada5) + (entradaASCII = entradaASCII + entrada5) : entrada5;
+
+    
+
+    //buzinar = printf("%b", (entradaASCII == verdadeiroASCII) ? true : false);
+
+    printf((entradaASCII == verdadeiroASCII) ? "Buzinaaaa\a\n" : (entradaASCII == falsoASCII) ?"Silêncio\n" : "Você digitou errado!\n");
+
+    return 0;
+}

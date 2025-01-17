9f735c86802c60f0e64378ce66a9542e11b48f68
Nodelay <121840258+Iraildo-Sousa@users.noreply.github.com>
2025-01-16 23:28:25 -0300

    Add files via upload

---
+++ Exercicios/Exercicio32.cpp
@@ -0,0 +1,16 @@
+/* Testando o tipo bool */
+
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char ** argv) {
+
+    bool verdadeiro = true, falso = false;
+
+    cout << sizeof(verdadeiro) << endl;
+    cout << sizeof(falso) << endl;
+    cout << sizeof(int) << endl;
+
+    return 0;
+}
